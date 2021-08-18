aee36425ab12b676f7d8886990721025d19ec847
Scott LaMott <slm@becks.ccb>
2021-08-18 23:09:59 +0200

    prompt nach oben

--- .local/plugins/slm-fzf.plugin.zsh
+++ .local/plugins/slm-fzf.plugin.zsh
@@ -1,7 +1,8 @@
-
+####
 export FZF_DEFAULT_COMMAND='fdfind --hidden --type=f --follow --exclude=.git'
-#export FZF_DEFAULT_OPTS="--info=inline --layout=reverse --prompt='exact >' -e"
-export FZF_DEFAULT_OPTS="--info=inline --layout=reverse --prompt='fuzzy >'"
+
+#export FZF_DEFAULT_OPTS="--layout=reverse --info=inline --layout=reverse --prompt='EXACT >' -e"
+export FZF_DEFAULT_OPTS="--layout=reverse --info=inline --layout=reverse --prompt='FUZZY >'"
 
 # Using highlight (http://www.andre-simon.de/doku/highlight/en/highlight.html)
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
